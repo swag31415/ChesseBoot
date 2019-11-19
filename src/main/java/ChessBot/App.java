ddb1e69a2dc912eabd768094dd5382415e134e09
swag31415 <kidswagatam18@gmail.com>
2019-11-19 03:05:07 -0700

    Imported stockfish

--- src/main/java/ChessBot/App.java
+++ src/main/java/ChessBot/App.java
@@ -3,12 +3,25 @@
  */
 package ChessBot;
 
-public class App {
-    public String getGreeting() {
-        return "Hello world.";
-    }
+import xyz.niflheim.stockfish.StockfishClient;
+import xyz.niflheim.stockfish.engine.enums.Query;
+import xyz.niflheim.stockfish.engine.enums.QueryType;
+import xyz.niflheim.stockfish.engine.enums.Variant;
+import xyz.niflheim.stockfish.exceptions.StockfishInitException;
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+public class App {
+    public static void main(String[] args) throws StockfishInitException {
+        StockfishClient client = new StockfishClient.Builder()
+        .setInstances(4)
+        .setVariant(Variant.BMI2)
+        .build();
+
+        Query query = new Query.Builder(QueryType.Legal_Moves)
+        .setFen("rnbqkbnr/pppppppp/8/8/8/8/RRRRRRRR/RNBQKBNR w KQkq - 0 1")
+        .build();
+
+        client.submit(query, result -> {
+            System.out.println(result);
+        });
     }
 }
