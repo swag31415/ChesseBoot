a4abbfa79d9d851f894e5d53aa7c199f7681722d
swag31415 <kidswagatam18@gmail.com>
2019-11-28 12:39:28 -0500

    Replaced JavaFx with hotkeys

--- src/main/java/ChessBot/App.java
+++ src/main/java/ChessBot/App.java
@@ -3,73 +3,98 @@
  */
 package ChessBot;
 
-import java.io.IOException;
-import java.net.URL;
+import java.awt.AWTException;
+import java.awt.Color;
+import java.awt.MouseInfo;
+import java.awt.Point;
+import java.awt.Robot;
+import java.util.Random;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.NativeHookException;
+import org.jnativehook.keyboard.NativeKeyEvent;
+import org.jnativehook.keyboard.NativeKeyListener;
 
-public class App extends Application {
+import xyz.niflheim.stockfish.exceptions.StockfishInitException;
 
-    private static Stage pStage;
-    private static App instance;
+public class App implements NativeKeyListener {
 
-    public enum fxmlFiles {
-        Splash("Splash.fxml");
+    private static final Color light = new Color(238, 238, 210);
+    private static final Color dark = new Color(118, 150, 86);
+    private static final Color light_selected = new Color(246, 246, 130);
+    private static final Color dark_selected = new Color(186, 202, 68);
 
-        private String file;
+    private static ChessBot chessBot;
+    private boolean running;
 
-        private fxmlFiles(String file) {
-            this.file = file;
+    public App() throws StockfishInitException {
+        App.chessBot = new ChessBot(light, dark, light_selected, dark_selected, 20, 800);
+        this.running = false;
+    }
+
+    public static void main(String[] args) throws NativeHookException, StockfishInitException {
+        GlobalScreen.registerNativeHook();
+        GlobalScreen.addNativeKeyListener(new App());
+    }
+
+    @Override
+    public void nativeKeyTyped(NativeKeyEvent nativeEvent) {
+        // Do Nothing
+    }
+
+    @Override
+    public void nativeKeyPressed(NativeKeyEvent nativeEvent) {
+        // Do Nothing
+    }
+
+    @Override
+    public void nativeKeyReleased(NativeKeyEvent nativeEvent) {
+        if (nativeEvent.getKeyCode() == NativeKeyEvent.VC_F6) {
+            this.running = !this.running;
+            this.jitterBot(50);
         }
 
-        public String get() {
-            return this.file;
+        if (this.running) {
+            switch (nativeEvent.getKeyCode()) {
+            case NativeKeyEvent.VC_I: // Pressed 'i'
+                App.chessBot.getBoard();
+            case NativeKeyEvent.VC_M: // Pressed 'm'
+                App.chessBot.getIsWhite();
+                App.chessBot.generatePieceMap();
+                this.jitterBot(100);
+                break;
+            case NativeKeyEvent.VC_C: // Pressed 'c'
+                App.chessBot.getIsWhite();
+                this.jitterBot(50);
+                break;
+            case NativeKeyEvent.VC_P: // Pressed 'p'
+                App.chessBot.makeMove();
+                this.jitterBot(5);
+                break;
+            case NativeKeyEvent.VC_ESCAPE: // Pressed esc
+                System.exit(0);
+                break;
+            default:
+                break;
+            }
         }
     }
 
-    public static void main(String[] args) {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        App.pStage = primaryStage;
-        primaryStage.setResizable(false);
-
-        App.loadScene(fxmlFiles.Splash, App.pStage);
-        primaryStage.show();
-    }
-
-    @Override
-    public void init() {
-        App.instance = this;
-    }
-
-    @Override
-    public void stop() throws Exception {
-        super.stop();
-        System.exit(0);
-    }
-
-    public static <T extends Controller> T loadScene(fxmlFiles file, Stage stage) throws IOException {
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(App.getResource(file.get()));
-        Pane pane = loader.<Pane>load();
-        stage.setScene(new Scene(pane));
-        return loader.getController();
-    }
-
-    public static <T extends Pane> T getFxml(fxmlFiles file) throws IOException {
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(App.getResource(file.get()));
-        return loader.<T>load();
-    }
-
-    public static URL getResource(String file) {
-        return App.instance.getClass().getClassLoader().getResource(file);
+    private void jitterBot(int c) {
+        int t = 10;
+        int s = 5;
+        Robot r;
+        try {
+            r = new Robot();
+        } catch (AWTException e) {
+            e.printStackTrace();
+            r = null;
+        }
+        Random ran = new Random();
+        Point origin = MouseInfo.getPointerInfo().getLocation();
+        r.setAutoDelay(t);
+        for (int i = 0; i < c; i++) {
+            r.mouseMove((int) origin.getX() + ran.nextInt(s), (int) origin.getY() + ran.nextInt(s));
+        }
     }
 }
